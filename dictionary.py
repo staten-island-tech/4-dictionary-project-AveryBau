3f3ae396d687d229378eefd0eeacc666c9d94656
AveryBau <averya34@nycstudents.net>
2026-02-12 09:44:37 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,26 @@
-item = {
+best_buy_items = {    
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-    
 }
+
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])
+          
+
+
+"""If you want just the name of the first item, you write:"""
+""" best_buy_items[0]["name"] """
+""" [0] means the first item in the list
+["name"] means “look inside that dictionary and grab the name. """
+
+""" If you want the price of the second item:
+best_buy_items[1]["price"] """
+""" 
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])
+
+index: the position in the list (0, 1, 2, 3, …).
+item: the dictionary at that position. """
+
