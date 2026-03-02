3bc2834c4162e01b32e7ed4d11a44f072d96a8ae
AveryBau <averya34@nycstudents.net>
2026-03-02 09:29:00 -0500

    Update assessmentpractice.py

--- assessmentpractice.py
+++ assessmentpractice.py
@@ -1,11 +1,12 @@
-
+""" 
 def occupied(n, y, t):
     found = 0
     for i in range (n):
         if y[i] == "C" and t[i] == "C":
             found += 1
     print(found)
-occupied(5, "CCCC.", "C...C")
+occupied(5, "C.C.C", "C..CC") """
+
 
 
 
@@ -13,3 +14,24 @@ occupied(5, "CCCC.", "C...C")
     return x + y
 result = add(5,6)
 print(result)"""
+
+
+def languages (sentence):
+    t=0
+    T=0
+    s=0
+    S=0
+    for i in range (sentence):
+        if sentence[i] == "t":
+            t += 1
+        if sentence[i] == "T":
+            T += 1
+        if sentence[i] == "s":
+            s =+ 1
+        if sentence[i] == "S":
+            S =+ 1
+    if s+S >= t+T:
+        print("probably English")
+    if t+T >= s+S:
+        print("probably French")
+languages(3, "The red cat sat on the mat. Why are you so sad cat? Don't ask that.")
